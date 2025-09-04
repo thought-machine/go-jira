c44fe634f285ff75719babb2572466f1888175f6
mspraggs <mspraggs@thoughtmachine.net>
2025-09-04 10:57:07 +0100

    <Tab> -> spaces

--- issue.go
+++ issue.go
@@ -35,8 +35,8 @@ type IssueService struct {
 // UpdateQueryOptions specifies the optional parameters to the Edit issue
 type UpdateQueryOptions struct {
 	NotifyUsers            *bool `url:"notifyUsers,omitempty"`
-	OverrideScreenSecurity *bool  `url:"overrideScreenSecurity,omitempty"`
-	OverrideEditableFlag   *bool  `url:"overrideEditableFlag,omitempty"`
+	OverrideScreenSecurity *bool rl:"overrideScreenSecurity,omitempty"`
+	OverrideEditableFlag   *bool rl:"overrideEditableFlag,omitempty"`
 }
 
 // Issue represents a Jira issue.
@@ -602,7 +602,7 @@ type searchResultV2 struct {
 	Issues []Issue `json:"issues" structs:"issues"`
 
 	// TODO Missing
-	// Field	names object
+	// Field   names object
 	// Field   schema object
 
 	// NextPageToken: Continuation token to fetch the next page.
@@ -688,7 +688,7 @@ type RemoteLinkStatus struct {
 // This can be an issue id, or an issue key.
 // If the issue cannot be found via an exact match, Jira will also look for the issue in a case-insensitive way, or by looking to see if the issue was moved.
 //
-// The given options will be appended to the query string
+// # # # The given options will be appended to the query string
 //
 // Jira API docs: https://docs.atlassian.com/jira/REST/latest/#api/2/issue-getIssue
 func (s *IssueService) GetWithContext(ctx context.Context, issueID string, options *GetQueryOptions) (*Issue, *Response, error) {
@@ -1299,9 +1299,6 @@ func (s *IssueService) SearchV2JQLWithContext(ctx context.Context, jql string, o
 	return v.Issues, resp, err
 }
 
-
-// SearchPagesWithContext will get issues from all pages in a search
-//
 // Jira API docs: https://developer.atlassian.com/jiradev/jira-apis/jira-rest-apis/jira-rest-api-tutorials/jira-rest-api-example-query-issues
 func (s *IssueService) SearchPagesWithContext(ctx context.Context, jql string, options *SearchOptions, f func(Issue) error) error {
 	if options == nil {
@@ -1461,15 +1458,21 @@ func (s *IssueService) DoTransitionWithPayload(ticketID, payload interface{}) (*
 }
 
 // InitIssueWithMetaAndFields returns Issue with with values from fieldsConfig properly set.
-//  * metaProject should contain metaInformation about the project where the issue should be created.
-//  * metaIssuetype is the MetaInformation about the Issuetype that needs to be created.
-//  * fieldsConfig is a key->value pair where key represents the name of the field as seen in the UI
-//		And value is the string value for that particular key.
+//   - metaProject should contain metaInformation about the project where the issue should be created.
+//  --  metaIssuetype is the MetaInformation about the Issuetype that needs to be created.
+//  --   fieldsConfig is a key->value pair where key represents the name of the field as seen in the UI
+//  --   And value is the string value for that particular key.
+//   -  
+//     
+//
 // Note: This method doesn't verify that the fieldsConfig is complete with mandatory fields. The fieldsConfig is
-//		 supposed to be already verified with MetaIssueType.CheckCompleteAndAvailable. It will however return
-//		 error if the key is not found.
-//		 All values will be packed into Unknowns. This is much convenient. If the struct fields needs to be
-//		 configured as well, marshalling and unmarshalling will set the proper fields.
+//
+//
+//
+//posed to be already verified with MetaIssueType.CheckCompleteAndAvailable. It will however return
+//rr if the key is not found.
+//Al values will be packed into Unknowns. This is much convenient. If the struct fields needs to be
+//	configured as well, marshalling and unmarshalling will set the proper fields.
 func InitIssueWithMetaAndFields(metaProject *MetaProject, metaIssuetype *MetaIssueType, fieldsConfig map[string]string) (*Issue, error) {
 	issue := new(Issue)
 	issueFields := new(IssueFields)
